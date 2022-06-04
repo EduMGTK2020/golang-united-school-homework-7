1880d0211956fbae593cc4f08ba4f083d53c5f41
Ed <mail@544062.ru>
2022-06-04 16:05:23 +0300

    Testing solution

--- students_test.go
+++ students_test.go
@@ -2,6 +2,9 @@ package coverage
 
 import (
 	"os"
+	"reflect"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -17,4 +20,109 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
+func TestPeoples(t *testing.T){
+	person0 := Person{"Artur", 	"Dow", time.Now()}
+	person1 := Person{"Artur", 	"Down", time.Now()}
+	person2 := Person{"John", 	"Down", time.Now().Add(time.Hour)}
+	person3 := Person{"Ara", 	"Down", time.Now()}
 
+	peoples := People{}
+	len0 := peoples.Len()
+	if len0 != 0 {
+		t.Errorf("Wrong get len: expected %d, got %d", 0, len0)		
+	}
+
+	peoples = append(peoples, person0)
+	len1 := peoples.Len()
+	if len1 != 1 {
+		t.Errorf("Wrong get len: expected %d, got %d", 0, len0)		
+	}
+
+	peoples = append(peoples, person1)
+	peoples.Swap(0,1)
+	if !reflect.DeepEqual(person0, peoples[1]) {
+		t.Errorf("Wrong swap: expected %v, got %v", person0, peoples[1])
+	}
+	if !reflect.DeepEqual(person1, peoples[0]) {
+		t.Errorf("Wrong swap: expected %v, got %v", person1, peoples[0])
+	} 
+
+	peoples = append(peoples, person2)
+	peoples = append(peoples, person3)
+	peoples.Swap(0,1)
+
+	testDataLess:= []struct {
+		i int
+		j int
+		expected bool
+	}{
+		{0,1, true},
+		{0,3, false},
+		{1,2, false},
+		{1,0, false},
+	}
+
+	for k, v:= range testDataLess {
+		got:= peoples.Less(v.i, v.j)
+		if got != v.expected {
+			t.Errorf("Step %d - wrong result: expected %v, got %v", k, v.expected, got)
+		}
+	}
+}
+
+func TestMatrix(t *testing.T) {
+
+	var matrix *Matrix
+	var err error
+
+	matrix, err = New("")
+	if matrix != nil {
+		t.Errorf("Wrong error result: expected %v, got %v", nil, err)		
+	}
+	_, err = New("0\n0 1")
+	if err == nil {
+		t.Errorf("Wrong error result: expected %v, got %v", nil, err)		
+	}
+	
+	matrix, err = New("0 1 2\n0 1 2")
+	if err != nil {
+		t.Errorf("Wrong error result: expected %v, got %v", nil, err)		
+	}
+	
+	testSet := []struct{
+		r int
+		c int
+		d int
+		set bool
+	}{
+		{0,0,1,true},
+		{3,3,1,false},
+		{-3,-3,1,false},
+	}
+
+	for k, v := range testSet {
+		got := matrix.Set(v.r, v.c, v.d)
+		if got != v.set {
+			t.Errorf("Step %d - wrong result: expected %v, got %v", k, v.set, got)			
+		}  
+	}
+	
+	expM := [][]int {
+		{1,1,2},
+		{0,1,2},
+	}
+
+	if !reflect.DeepEqual(matrix.Rows(), expM) {
+		t.Errorf("Wrong result: expected %v, got %v", expM, matrix.Rows()  )		
+	}
+
+	expM = [][]int {
+		{1,0},
+		{1,1},
+		{2,2},
+	}
+
+	if !reflect.DeepEqual(matrix.Cols(), expM) {
+		t.Errorf("Wrong result: expected %v, got %v", expM, matrix.Cols())		
+	}
+}
